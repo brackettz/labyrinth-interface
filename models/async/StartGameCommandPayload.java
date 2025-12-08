5318e50170e5cff992e864c6b9111fe2c5be01e9
Aaron <aaronkalupa@outlook.com>
2025-12-08 19:28:02 +0100

    Refine StartGameCommandPayload schema by updating required fields and adding default values for gameDurationInSeconds

--- models/async/StartGameCommandPayload.java
+++ models/async/StartGameCommandPayload.java
@@ -6,9 +6,9 @@ import java.util.Objects;
 public class StartGameCommandPayload {
   private CommandType type;
   private BoardSize boardSize;
-  private int treasureCardCount = 24;
+  private Integer treasureCardCount = 24;
   private Integer totalBonusCount;
-  private Integer gameDurationInSeconds;
+  private Integer gameDurationInSeconds = 3600;
   private Map<String, Object> additionalProperties;
 
   public CommandType getType() { return this.type; }
@@ -17,8 +17,8 @@ public class StartGameCommandPayload {
   public BoardSize getBoardSize() { return this.boardSize; }
   public void setBoardSize(BoardSize boardSize) { this.boardSize = boardSize; }
 
-  public int getTreasureCardCount() { return this.treasureCardCount; }
-  public void setTreasureCardCount(int treasureCardCount) { this.treasureCardCount = treasureCardCount; }
+  public Integer getTreasureCardCount() { return this.treasureCardCount; }
+  public void setTreasureCardCount(Integer treasureCardCount) { this.treasureCardCount = treasureCardCount; }
 
   public Integer getTotalBonusCount() { return this.totalBonusCount; }
   public void setTotalBonusCount(Integer totalBonusCount) { this.totalBonusCount = totalBonusCount; }
