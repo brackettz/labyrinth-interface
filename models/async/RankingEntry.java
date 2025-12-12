4295b78645ae5accf40fd289e9420a4b8ebfcb28
Thomas Klammer <thomasklammer.1993@gmail.com>
2025-12-12 11:18:46 +0100

    removed unnecessary stuff in game over event

--- models/async/RankingEntry.java
+++ models/async/RankingEntry.java
@@ -4,16 +4,12 @@ import java.util.Map;
 import java.util.Objects;
 public class RankingEntry {
   private String playerId;
-  private Integer rank;
   private Integer score;
   private PlayerGameStats stats;
   private Map<String, Object> additionalProperties;
 
   public String getPlayerId() { return this.playerId; }
   public void setPlayerId(String playerId) { this.playerId = playerId; }
-
-  public Integer getRank() { return this.rank; }
-  public void setRank(Integer rank) { this.rank = rank; }
 
   public Integer getScore() { return this.score; }
   public void setScore(Integer score) { this.score = score; }
@@ -35,7 +31,6 @@ public class RankingEntry {
     RankingEntry self = (RankingEntry) o;
       return 
         Objects.equals(this.playerId, self.playerId) &&
-        Objects.equals(this.rank, self.rank) &&
         Objects.equals(this.score, self.score) &&
         Objects.equals(this.stats, self.stats) &&
         Objects.equals(this.additionalProperties, self.additionalProperties);
@@ -43,14 +38,13 @@ public class RankingEntry {
 
   @Override
   public int hashCode() {
-    return Objects.hash((Object)playerId, (Object)rank, (Object)score, (Object)stats, (Object)additionalProperties);
+    return Objects.hash((Object)playerId, (Object)score, (Object)stats, (Object)additionalProperties);
   }
 
   @Override
   public String toString() {
     return "class RankingEntry {\n" +   
       "    playerId: " + toIndentedString(playerId) + "\n" +
-      "    rank: " + toIndentedString(rank) + "\n" +
       "    score: " + toIndentedString(score) + "\n" +
       "    stats: " + toIndentedString(stats) + "\n" +
       "    additionalProperties: " + toIndentedString(additionalProperties) + "\n" +
